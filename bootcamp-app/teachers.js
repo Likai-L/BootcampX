7c8b731e12c605132769186256eed19648a8c5c9
Likai Liu <likailiu2@gmail.com>
2022-12-15 18:16:30 +0800

    Implement a terminal APP that, given a cohort name, returns the teachers who assisted during this cohort

---
+++ bootcamp-app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'likailiu',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2] || 'JUL02';
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${cohortName}'
+ORDER BY teacher;
+`)
+  .then(res => {
+    console.log("connected");
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    });
+  });
